Fix normalization of predictions

learn.py
@@ -12,9 +12,53 @@ import phoment
 import gbr
 
 
-alr = aligner.Aligner(feature_file='latin_features.txt', sub_penalty=4.0, tolerance=1.0)
+TRAINFILE = 'latin_paradigms.txt'
+CONFILE = 'latin_constraints.txt'
+FEATUREFILE = 'latin_features.txt'
 
-lex = gbr.Lexicon('example_paradigms.txt')
+
+def normalize_exp(exp_probs, candidates):
+    normalized = []
+    lexeme = ''
+    these_probs = []
+    for p, c in zip(exp_probs, candidates):
+        if c[0] == lexeme:
+            these_probs.append(p)
+        else:
+            psum = sum(these_probs)
+            normalized += [p/psum if psum > 0 else p for p in these_probs]
+            lexeme = c[0]
+            these_probs = [p]
+    normalized += [p/psum if psum > 0 else p for p in these_probs]
+    return np.array(normalized)
+
+def calc_base_regularization(labeled_weights):
+    result = 0
+    bases = set([label[0] for weight, label in labeled_weights])
+    for base in bases:
+        these_weights = [weight for weight, label in labeled_weights if label[0] == base]
+        result += sum([abs(w1-w2) for w1, w2 in itertools.combinations(these_weights, 2)])
+    return result
+
+def objective(weights, weight_labels, deriv_data, l1_mult=0.01, l2_mult=0.01, base_reg_mult=0.01):
+    norms_by_deriv = []
+    labeled_weights = list(zip(weights, weight_labels))
+    for d in deriv_data:
+        these_weights = [w for w, label in labeled_weights if label[1] == d and label[0] in deriv_data[d]['bases']]
+        exp_probs = scipy.dot(deriv_data[d]['predicted'], these_weights)
+        norms_by_deriv.append(np.linalg.norm(exp_probs-deriv_data[d]['observed']))
+
+        l1l2_terms = l1_mult*sum(weights) + l2_mult*sum([w**2 for w in weights])
+        base_reg_term = base_reg_mult*calc_base_regularization(labeled_weights)
+
+    return sum(norms_by_deriv) + l1l2_terms + base_reg_term
+
+
+
+## Initialize aligner and lexicon
+alr = aligner.Aligner(feature_file=FEATUREFILE, sub_penalty=4.0, tolerance=1.0)
+
+lex = gbr.Lexicon(TRAINFILE)
 
 ## Create list of *OBSERVED* MPS tuples
 lex.create_cells()
@@ -61,7 +105,7 @@ for one_mapping in sll_inputs:
     #     print(s)
     #     print(s.associated_forms)
 
-    with open('example_constraints.txt') as con_file:
+    with open(CONFILE) as con_file:
         conreader = csv.reader(con_file, delimiter='\t')
         constraints = [c[0] for c in conreader if len(c) > 0]
 
@@ -87,12 +131,12 @@ for deriv_cell in full_data:
     this_deriv_matrix = []
     this_deriv_obs = []
     this_deriv_bases = [b for b in lex.cells if b != deriv_cell]
+    this_deriv_candidates = []
     with open('{}.txt'.format(str(deriv_cell)), 'w') as outf:
         outf.write('lexeme\tform\tobserved\t'+'\t'.join([str(b) for b in this_deriv_bases])+'\n')
-        # print()
-        # print(deriv_cell)
         for lexeme in full_data[deriv_cell]:
             for candidate in full_data[deriv_cell][lexeme]:
+                this_deriv_candidates.append((lexeme, candidate))
                 # get observed probability
                 row = ['{}\t{}'.format(str(lexeme), candidate)]
                 obs_or_not = lex.cells[deriv_cell][lexeme] == candidate
@@ -106,32 +150,37 @@ for deriv_cell in full_data:
                 this_deriv_matrix.append(predicted_probs)
                 row += [str(fl) for fl in predicted_probs]
                 outf.write('\t'.join(row)+'\n')
-    deriv_data[deriv_cell] = {'observed': np.array(this_deriv_obs), 'predicted': np.array(this_deriv_matrix), 'bases': this_deriv_bases}
+    deriv_data[deriv_cell] = {'observed': np.array(this_deriv_obs), 'predicted': np.array(this_deriv_matrix), 'bases': this_deriv_bases, 'candidates': this_deriv_candidates}
 
 
 ## Learn weights
 posReals = [(0,25) for wt in range(len(lex.gbr_features))]
 
-#### NEXT: get the objective function to properly assign weights, add Paul's regularization term
-
-def objective(weights, weight_labels, deriv_data, l1_mult=0.0, l2_mult=0.0):
-    norms_by_deriv = []
-    labeled_weights = list(zip(weights, weight_labels))
-    for d in deriv_data:
-        for w, label in labeled_weights[:]:
-            these_weights = [w for w, label in labeled_weights if label[1] == d and label[0] in deriv_data[d]['bases']]
-        exp_probs = scipy.dot(deriv_data[d]['predicted'], these_weights)
-        norms_by_deriv.append(np.linalg.norm(exp_probs-deriv_data[d]['observed']))
-    return sum(norms_by_deriv) + l1_mult*sum(weights) + l2_mult*sum([w**2 for w in weights])
-
-
 output_weights, nfeval, return_code = scipy.optimize.fmin_l_bfgs_b( 
         objective, scipy.rand(len(lex.gbr_features)), 
-        args=(lex.gbr_features, deriv_data, 0.1, 0.1),
+        args=(lex.gbr_features, deriv_data, 0.01, 0.01, 0.01),
         bounds=posReals,
         approx_grad=True)
 
 
+## Print model predictions
+# print('\n\nPredictions:\n')
+# labeled_output_weights = list(zip(output_weights, lex.gbr_features))
+# for d in deriv_data:
+#     print(d)
+#     these_weights = [w for w, label in labeled_output_weights if label[1] == d and label[0] in deriv_data[d]['bases']]
+#     exp_probs = scipy.dot(deriv_data[d]['predicted'], these_weights)
+#     normalized_exp_probs = normalize_exp(exp_probs, deriv_data[d]['candidates'])
+#     this_lexeme = ''
+#     for c, o, e in zip(deriv_data[d]['candidates'], deriv_data[d]['observed'], normalized_exp_probs):
+#         if c[0] != this_lexeme:
+#             print('')
+#             this_lexeme = c[0]
+#         print('{}\t{}\t\t{}\t{}'.format(c[0], c[1], str(o), str(e)))
+#     print('\n')
+
+
+## Print weights
 print('\n\nWeights:')
 labeled_weights = list(zip(output_weights, lex.gbr_features))
 for d in deriv_data:
@@ -139,5 +188,3 @@ for d in deriv_data:
     for w, label in labeled_weights[:]:
         if label[1] == d and label[0] in deriv_data[d]['bases']:
             print('{}: {}'.format(str(label[0]), str(w)))
-
-